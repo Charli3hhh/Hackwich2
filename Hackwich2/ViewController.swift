89af8f0a5a4a14013bd76b93634f3467e2cdcc3c
Charles Ombao <charlesb.ombao@gmail.com>
2020-01-28 10:42:12 -1000

    Completed parts 1-7

--- Hackwich2/ViewController.swift
+++ Hackwich2/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+   
+    @IBAction func PressMeButtonPressed(_ sender: Any)
+    {
+        print("Hello World")
+        
+    }
+    
 }
 
